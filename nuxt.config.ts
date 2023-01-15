9f2d63cf703805babef75b75e456ca4fbfde883e
jimmy <s10219505@connect.np.edu.sg>
2023-01-15 18:55:18 +0800

    feat: add meta information

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,5 +10,18 @@ export default defineNuxtConfig({
             'entity',
             'commands'
         ]
+    },
+    app: {
+        head: {
+            charset: 'utf-16',
+            viewport: 'width=device-width, initial-scale=1',
+            title: '✨ Hemera ✨',
+            meta: [
+              { name: 'description', content: 'Heroes of Ancient Legends Stat Calculator.' },
+            ],
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' },
+            ]
+        }
     }
 })
