643b1273ac8c3ddfe77892d221ff8c54a0ff1aa0
jimmy <s10219505@connect.np.edu.sg>
2023-01-19 17:57:49 +0800

    refactor: generalize table filter function

--- utils/filterKeyData.ts
+++ utils/filterKeyData.ts
@@ -1,6 +1,3 @@
-export const filterKeyData = (element: [string, any], index?: number, array?: [string, any][]) => {
-    return element[0] !== 'cooldown'
-    && element[0] !== 'damage'
-    && element[0] !== 'healingCooldown'
-    && element[0] !== 'healing'
+export const filterKeyData = <T extends string = any>(element: T[], index: number, array: T[][]) => {
+    return !['cooldown', 'damage', 'healingCooldown', 'healing'].includes(element[0])
 }
